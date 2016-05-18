2f25776b470e98c35ea40ef4c12352dbdfdbbbea
Tomasz Jezierski <deco.jezierski@gmail.com>
2016-05-18 20:02:47 +0200

    Map resize

--- wysadzulice/assets/js/home.js
+++ wysadzulice/assets/js/home.js
@@ -3,16 +3,22 @@ import { Model, View, Collection} from 'backbone';
 
 const BannerView = View.extend({
 
-  initialize: function(options){
+  initialize: function(options) {
     this.parent = options.parent;
     this.setElement(jquery('#home-banner'));
-    var markers = this.parent.collection.toApi();
-    const apiKey = 'https://maps.googleapis.com/maps/api/staticmap?size=400x300&scale=2&key=AIzaSyCRnnZblqETt7ivbluhDXDuF3B0HrbdVnI' + markers;
-    this.$el.html('<div id="banner-container"><img src="' + apiKey +'"></div>');
+    const markers = this.parent.collection.toApi();
+
+    const apiKey = 'https://maps.googleapis.com/maps/api/staticmap?size=400x300&key=AIzaSyCRnnZblqETt7ivbluhDXDuF3B0HrbdVnI' + markers;
+    this.$el.html(`<div id="banner-container"><img src="${apiKey}"></div>`);
   },
 
-  //render: function(data){
-  //}
+});
+
+const SlideView = View.extend({
+  initialize: function(options, slide) {
+    this.parent = options.parent;
+    this.setElement(jquery('#home-slider'));
+  },
 });
 
 
@@ -20,7 +26,7 @@ const CampaignModel = Model.extend({
   defaults: {
     id: null,
     lat: null,
-    lng: null
+    lng: null,
   },
 });
 
@@ -28,33 +34,33 @@ const CampaignsCollection = Collection.extend({
   model: CampaignModel,
   url: '/api/campaigns/',
 
-  toApi: function(){
-    var apiString = "";
+  toApi: function() {
+    let apiString = '';
 
-    this.map(function (location) {
+    this.map(function(location) {
       const lat = location.attributes.lat;
       const lng = location.attributes.lng;
-      apiString = apiString + "&markers=color:blue%7Clabel:S%7C" + lat + "," + lng;
+      apiString = apiString + '&markers=color:blue%7Clabel:S%7C' + lat + ',' + lng;
     });
 
     return apiString;
-  }
+  },
 });
 
 export default View.extend({
 
-  initialize: function (options) {
+  initialize: function() {
     this.setElement(jquery('#home'));
     this.collection = new CampaignsCollection();
-    this.collection.fetch().then(this.render.bind(this));
+    this.listenTo(this.collection, 'add remove reset change', this.render);
+    this.collection.fetch();
   },
 
-  render: function(){
-
+  render: function() {
     const bannerView = new BannerView({
       parent: this,
     });
     this.$el.html(bannerView.render().el);
-  }
+  },
 
 });
